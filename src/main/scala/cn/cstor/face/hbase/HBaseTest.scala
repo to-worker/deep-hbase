7e83a56a3447705676e51ace9dee9568c1001459
feng.wei <alferwei98@163.com>
2017-01-06 12:47:44 +0800

    first commit

---
+++ src/main/scala/cn/cstor/face/hbase/HBaseTest.scala
@@ -0,0 +1,14 @@
+package cn.cstor.face.hbase
+
+/**
+  * Created on 2017/1/6
+  *
+  * @author feng.wei
+  */
+object HBaseTest {
+
+    def main(args: Array[String]) {
+        println("++++++++++")
+    }
+
+}

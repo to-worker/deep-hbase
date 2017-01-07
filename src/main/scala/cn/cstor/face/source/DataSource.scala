fbfb6f88a119427bf01ed903fe8178d4d72eed0e
feng.wei <alferwei98@163.com>
2017-01-07 17:46:52 +0800

    annotation

--- src/main/scala/cn/cstor/face/source/DataSource.scala
+++ src/main/scala/cn/cstor/face/source/DataSource.scala
@@ -3,6 +3,7 @@ package cn.cstor.face.source
 import java.io.FileInputStream
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 /**
   * Created on 2017/1/7
@@ -12,9 +13,14 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook
 object DataSource {
 
     def main(args: Array[String]) {
-        readXls("E:\\cstor\\deep_compare\\data\\test.xlsx")
+        readXlsx("E:\\cstor\\deep_compare\\data\\test.xlsx")
     }
 
+    /**
+      * HSSFWorkbook: implementation of the Excel '97(-2007) file format.
+      *
+      * @param path
+      */
     def readXls(path: String): Unit = {
         val hSSFWorkbook = new HSSFWorkbook(
             new FileInputStream(path))
@@ -24,6 +30,31 @@ object DataSource {
             println(rowNum)
         }
 
+    }
+
+    /**
+      * XSSF is the POI Project's pure Java implementation of the Excel 2007 OOXML (.xlsx) file format.
+      *
+      * @param path
+      */
+    def readXlsx(path: String): Unit = {
+        val workbook = new XSSFWorkbook(
+            new FileInputStream(path))
+
+        val sheet = workbook.getSheet("year")
+        for (i <- sheet.getFirstRowNum until (sheet.getLastRowNum)) {
+            val row = sheet.getRow(i)
+
+            if (row != null) {
+                for (j <- row.getFirstCellNum.toInt until (row.getLastCellNum)) {
+                    val cell = row.getCell(j)
+
+                    if (cell != null) {
+                        println(cell.getRawValue)
+                    }
+                }
+            }
+        }
     }
 
 }
